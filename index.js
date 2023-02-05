b64206bc4a6a5c49d9e8821c20bc09e45f9ae5fc
SumiAkter2 <akteralisha36@gmail.com>
2023-02-05 12:44:45 +0600

    connect mongo and insertOne

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const { CURSOR_FLAGS } = require("mongodb");
@@ -7,8 +8,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://dBusers:eGPoPbZyvdc8EeAl@cluster0.hnxcxtq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
   try {
+    const usersCollection = client.db("nodeMongo").collection("users");
+    const user = {
+      name: "Sumi Akter",
+      vill: "Bogura",
+    };
+    const result = await usersCollection.insertOne(user);
+    console.log(result);
   } finally {
   }
 }
